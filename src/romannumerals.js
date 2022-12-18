dec27ba3d42628e8cc0fb19baf473465db44e844
danielatwell2k5@msn.com <danielatwell2k5@msn.com>
2022-12-18 23:14:28 +0000

    Three passing tests. Numbers up to 9

--- src/romannumerals.js
+++ src/romannumerals.js
@@ -1,5 +1,35 @@
 function convertDecimal(num) {
-    return 'I';
+    let numeralString = '';
+    switch(num) {
+        case 1:
+            numeralString = 'I';
+            break;
+        case 2:
+            numeralString = 'II';
+            break;
+        case 3:
+            numeralString = 'III';
+            break;
+        case 4:
+            numeralString = 'IV';
+            break;
+        case 5:
+            numeralString = 'V';
+            break;
+        case 6:
+            numeralString = 'VI';
+            break;
+        case 7:
+            numeralString = 'VII';
+            break;
+        case 8:
+            numeralString = 'VIII';
+            break;
+        case 9:
+            numeralString = 'IX';
+            break;
+    }
+    return numeralString;
 }
 
 module.exports = convertDecimal;
