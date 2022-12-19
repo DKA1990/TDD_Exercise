e91187be8edc71e6437d58cba865eb7d5465ba12
danielatwell2k5@msn.com <danielatwell2k5@msn.com>
2022-12-19 14:26:00 +0000

    Second passing test. Converts 1 through 9

--- src/romannumerals.test.js
+++ src/romannumerals.test.js
@@ -3,3 +3,15 @@ const convertDecimal = require('./romannumerals')
 test('check function converts simplest number (1)', () => {
     expect(convertDecimal(1)).toBe('I');
 });
+
+test('check function converts numbers 1 through 9', () => {
+    expect(convertDecimal(1)).toBe('I');
+    expect(convertDecimal(2)).toBe('II');
+    expect(convertDecimal(3)).toBe('III');
+    expect(convertDecimal(4)).toBe('IV');
+    expect(convertDecimal(5)).toBe('V');
+    expect(convertDecimal(6)).toBe('VI');
+    expect(convertDecimal(7)).toBe('VII');
+    expect(convertDecimal(8)).toBe('VIII');
+    expect(convertDecimal(9)).toBe('IX');
+});
